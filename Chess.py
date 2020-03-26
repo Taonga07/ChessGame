d135ff3f21c142a8a0fa8209cecbb4d729143ec1
DigiHome <digihome@hbb-HP-255-G3-Notebook-PC.home>
2020-03-26 16:38:51 +0000

    somthing

---
+++ Chess.py
@@ -0,0 +1,26 @@
+import tkinter, Rules, os
+
+def set_up_window():
+    window = tkinter.Tk()
+    window.title('chess')
+    window.tk.call('wm', 'iconphoto', window._w, tkinter.PhotoImage(file = Rules.path +'Black_King.gif'))
+    play_chess(window)
+    
+def play_chess(window):
+    #reset_board(board)
+    #create_board(window, board)
+    window.mainloop()
+
+def reset_board(board):
+    pass
+    #return board
+
+def create_board(window, board):
+    pass
+
+if __name__ =="__main__":
+    set_up_window()
+    window.title('chess')
+    window.tk.call('wm', 'iconphoto', window._w, tkinter.PhotoImage(file = path +'Black_King.gif'))
+    
+    
---
+++ Rules.py
@@ -0,0 +1,3 @@
+import os
+
+path = os.getcwd() + '/Chess_Resources/'
