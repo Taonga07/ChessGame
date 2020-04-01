9a14a7820e2bb4900a1b7e49bda8490c488b0d23
Connor Sadler <connor.sadler@gmail.com>
2020-04-01 19:23:05 +0100

    Connor UI fixes Weds

--- Chess.py
+++ Chess.py
@@ -6,15 +6,30 @@ def set_up_window():
     window.tk.call('wm', 'iconphoto', window._w, tkinter.PhotoImage(file = Rules.path +'icon.gif'))
     #play_chess(window)
     start(window)
+    return window
+
 
 def start(window):
     board = reset_board()
+
+    # Can explicitly set window size here if you like
+    #window.geometry('400x400')
+
     photo = tkinter.PhotoImage(file = Rules.path + "Intro.gif")
     w = tkinter.Label(window, image = photo)
+    # This tip keeps the image around, otherwise it doesnt display properly
+    # Found it by a Google: http://effbot.org/pyfaq/why-do-my-tkinter-images-not-appear.htm
+    w.image = photo
+
+    # Thomas: If you want to use "pack" here and then also use a grid later when you create the board
+    #         then we need to remove all widgets from the window before we draw the board - see destroy_all_widgets
+    #         or you can use "w.grid(column=0, row=0)" instead of the pack
     w.pack()
+
     ent = tkinter.Entry(window)
     ent.pack()
     ent.focus_set() 
+
     menubar = tkinter.Menu(window)
 
     filemenu = tkinter.Menu(menubar, tearoff = 0)
@@ -23,12 +38,13 @@ def start(window):
     toolmenu = tkinter.Menu(menubar, tearoff = 0)
     helpmenu = tkinter.Menu(menubar, tearoff = 0)
 
-    filemenu.add_command(label="New", command = play_chess(window))
-    filemenu.add_command(label="Open", command = Files.onOpen(window, board))
-    filemenu.add_command(label="Save", command = Files.onSave(board)
-    filemenu.add_command(label="Exit", command = window.destroy())
+    # Thomas: You need to use "lambda:" here so it only runs the command when the menu option is clicked, rather than running it immediately
+    filemenu.add_command(label="New", command = lambda: play_chess(window))
+    filemenu.add_command(label="Open", command = lambda: Files.onOpen(window, board))
+    filemenu.add_command(label="Save", command = lambda: Files.onSave(board))
+    filemenu.add_command(label="Exit", command = lambda: window.destroy())
 
-    helpmenu.add_command(label="Open Guide", command = Files.openGuide())
+    helpmenu.add_command(label="Open Guide", command = lambda: Files.openGuide())
 
     menubar.add_cascade(label="File", menu = filemenu)
     menubar.add_cascade(label="Edit", menu = editmenu)
@@ -38,10 +54,15 @@ def start(window):
 
     window.config(menu = menubar)
 
+# https://stackoverflow.com/questions/15781802/python-tkinter-clearing-a-frame
+def destroy_all_widgets(window):
+    for widget in window.winfo_children():
+        widget.destroy()
+
 def play_chess(window):
+    destroy_all_widgets(window)
     board = reset_board()
     create_board(window, board)
-    window.mainloop()
 
 def reset_board():
     board = []
@@ -85,4 +106,5 @@ def create_board(window, board):
 
 
 if __name__ =="__main__":
-    set_up_window()
+    window = set_up_window()
+    window.mainloop()
