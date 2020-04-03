ac58cb1ab3d6f83b1418244cb0da5a04700dcd88
DigiHome <digihome@hbb-HP-255-G3-Notebook-PC.home>
2020-04-03 10:13:53 +0100

    minor edits. However menubar now on chess board screen!

--- Chess.py
+++ Chess.py
@@ -6,29 +6,17 @@ def set_up_window():
     window.tk.call('wm', 'iconphoto', window._w, tkinter.PhotoImage(file = Rules.path +'icon.gif'))
     #play_chess(window)
     start(window)
+    menu(window)
     return window
 
-
 def start(window):
-    board = reset_board()
-
-    # Can explicitly set window size here if you like
-    #window.geometry('400x400')
-
     photo = tkinter.PhotoImage(file = Rules.path + "Intro.gif")
     w = tkinter.Label(window, image = photo)
-    # This tip keeps the image around, otherwise it doesnt display properly
-    # Found it by a Google: http://effbot.org/pyfaq/why-do-my-tkinter-images-not-appear.htm
     w.image = photo
-
-    # Thomas: If you want to use "pack" here and then also use a grid later when you create the board
-    #         then we need to remove all widgets from the window before we draw the board - see destroy_all_widgets
-    #         or you can use "w.grid(column=0, row=0)" instead of the pack
     w.pack()
 
-    ent = tkinter.Entry(window)
-    ent.pack()
-    ent.focus_set() 
+def menu(window):
+    board = reset_board()
 
     menubar = tkinter.Menu(window)
 
@@ -38,7 +26,6 @@ def start(window):
     toolmenu = tkinter.Menu(menubar, tearoff = 0)
     helpmenu = tkinter.Menu(menubar, tearoff = 0)
 
-    # Thomas: You need to use "lambda:" here so it only runs the command when the menu option is clicked, rather than running it immediately
     filemenu.add_command(label="New", command = lambda: play_chess(window))
     filemenu.add_command(label="Open", command = lambda: Files.onOpen(window, board))
     filemenu.add_command(label="Save", command = lambda: Files.onSave(board))
@@ -54,13 +41,14 @@ def start(window):
 
     window.config(menu = menubar)
 
-# https://stackoverflow.com/questions/15781802/python-tkinter-clearing-a-frame
 def destroy_all_widgets(window):
     for widget in window.winfo_children():
-        widget.destroy()
+        if widget.winfo_class() != 'menubar':
+            widget.destroy()
 
 def play_chess(window):
     destroy_all_widgets(window)
+    menu(window)
     board = reset_board()
     create_board(window, board)
 
