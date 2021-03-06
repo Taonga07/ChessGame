c3aa39cb128ef1ef6b59c82b81d3b65c94d0fa0e
Thomas WS <Taongaws07@gmail.com>
2021-03-06 16:41:41 +0000

    renamed CC.py to CDpy

--- Chess.py
+++ Chess.py
@@ -1,4 +1,4 @@
-import tkinter, File, CC, CP, os
+import tkinter, File, CD, CP, os
 from tkinter import messagebox
 
 def set_up_window():
@@ -61,10 +61,10 @@ def layout_board(window, board):
     for column_number in range(0, 8):
         for row_number in range(0, 8):
             if board[column_number][row_number] == None:
-                square = tkinter.Label(window, text = "                 \n\n\n", bg = CC.bttnclrs[CC.bttnclr_turn])
+                square = tkinter.Label(window, text = "                 \n\n\n", bg = CD.bttnclrs[CD.bttnclr_turn])
             else:
                 img = tkinter.PhotoImage(file = board[column_number][row_number].icon)
-                square = tkinter.Label(window, bg = CC.bttnclrs[CC.bttnclr_turn], image = img)
+                square = tkinter.Label(window, bg = CD.bttnclrs[CD.bttnclr_turn], image = img)
                 square.image = img
 
             # Connor added - remove any existing grid item at this location - stops UI memory leak
@@ -72,8 +72,8 @@ def layout_board(window, board):
 
             square.grid(row = row_number, column = column_number, sticky = tkinter.N+tkinter.S+tkinter.W+tkinter.E)
             square.bind("<Button-1>", lambda event, data=window, data1 = board: on_click(event, data, data1))
-            CC.bttnclr_turn = 1-CC.bttnclr_turn
-        CC.bttnclr_turn = 1-CC.bttnclr_turn
+            CD.bttnclr_turn = 1-CD.bttnclr_turn
+        CD.bttnclr_turn = 1-CD.bttnclr_turn
 
 def CheckForCheck(board, colour):
     check_pieces = []
@@ -101,37 +101,37 @@ def CheckForCheck(board, colour):
         tkinter.messagebox.showinfo('Checkmate', f'{colour} wins! Well Done!')
 
 def on_click(event, window, board):
-    CC.onclick = 1 - CC.onclick
+    CD.onclick = 1 - CD.onclick
     square = event.widget
     row_number = int(square.grid_info()["row"])
     column_number  = int(square.grid_info()["column"])
     print(f"on_click {row_number}, {column_number}")
     square_clicked = (row_number, column_number)
     piece_clicked = board[column_number][row_number]
-    if CC.onclick == 0: # this is our fist click we are selecting the piece we want to move
-        if (piece_clicked != None)and(((CC.turn == 0)and(piece_clicked.colour == 'White'))or((CC.turn == 1)and(piece_clicked.colour == 'Black'))):
+    if CD.onclick == 0: # this is our fist click we are selecting the piece we want to move
+        if (piece_clicked != None)and(((CD.turn == 0)and(piece_clicked.colour == 'White'))or((CD.turn == 1)and(piece_clicked.colour == 'Black'))):
             ##CheckForCheck(board, piece_clicked.colour) # check for check/checkmate
-            CC.square_clicked = square_clicked #row_number,column_number
+            CD.square_clicked = square_clicked #row_number,column_number
             piece_clicked.possible_moves = [] # reset posible moves
             piece_clicked.find_moves(board)
-            CC.old_click = square_clicked
+            CD.old_click = square_clicked
             piece_clicked.highlight_moves(window, board)
         else: # if there is no piece or wrong colour piece where we clicked
             tkinter.messagebox.showinfo("Move Not Allowed","No/Your piece there, try again")
-            CC.onclick = 1 - CC.onclick
+            CD.onclick = 1 - CD.onclick
     else: # this is our second click, we are selecting the square to move to
-        row, column = CC.old_click
+        row, column = CD.old_click
         old_piece = board[row][column]
         if square_clicked not in old_piece.possible_moves: # check possible move for piece
             tkinter.messagebox.showinfo("Move Not Allowed", "Your piece can not move there!")
             layout_board(window, board) #reset board
             return
-        board[column_number][row_number] = board[CC.old_click[0]][CC.old_click[1]]
+        board[column_number][row_number] = board[CD.old_click[0]][CD.old_click[1]]
         board[column_number][row_number].row = row_number
         board[column_number][row_number].column = column_number
-        board[CC.old_click[0]][CC.old_click[1]] = None
+        board[CD.old_click[0]][CD.old_click[1]] = None
         layout_board(window, board) #reset board
-        CC.turn = 1 - CC.turn
+        CD.turn = 1 - CD.turn
 
 if __name__ =="__main__":
     play_chess('Test.txt')
