42cf5dcec210c1e857f19a65d957021ceac535b3
Ade-StapleHill <ade.perry@digilocal.org.uk>
2021-10-21 15:42:20 +0100

    Added command parsing e.g. 'Qc3:b2'
    test1.py : test6 is expected fail as 'Check' not reported

--- ChessHeadless.py
+++ ChessHeadless.py
@@ -1,3 +1,4 @@
+import re
 from Pieces import Pawn, Rook, Bishop, Queen, King, Knight
 
 class ChessExc(Exception):
@@ -17,7 +18,7 @@ class InvMoveExc(ChessExc):
 
 class ChessHeadless():
     def __init__(self, file='New_Game.txt'):
-        self.board, self.turn = self.read_game_data(file)
+        self.board, self.turn = self.read_game_data(file) # file maybe None
         self.click, self.first_click = 1, (0, 0)
     
     def get_piece(self, row, col):
@@ -67,17 +68,148 @@ class ChessHeadless():
         from_piece = self.movefrom(*from_pos)
         to_piece = self.moveto(*to_pos)
         return to_piece
+    
+    def new_board(self):
+        board = [[None]*8 for row in range(8)]
+        return board
+    
+    def notation_pos(self, token):
+        # Return tuple (row, col) from parsed token, else None
+        # String fmt :: '(<row>, <col>)' | '[a-h][1-8]'
+        #   first char row:[a-h], second char col:[1-8]
+        #      eg. 'a8' == (0,0), 'a1' == (7,0), 'h1' == (7,7), 'h8' == (0,7)
+        pos = None
+        row = -1; col = -1
+
+        if len(token) == 2:
+            token.lower()
+            col = ord(token[0]) - ord('a') # col 'a' is table column 0
+            row = 8 - int(token[1])          # row 1 is table row 7
+        else:
+            if re.match(r'\([0-7],[0-7]\)$', token):
+                row = int(token[1])
+                col = int(token[3])
+        if row >= 0 and row < 8 and col >= 0 and col < 8:
+            pos = (row, col)
+        return pos
+    
+    def notation_piece(self, token):
+        # Return tuple (<class>, colour) if token starts with a piece abbrv, else None.
+        # one char abbrv of piece (R N B Q K P) or 'D' for board.dump, 
+        # upper case is white, lower black e.g. N is white knight, n is black
+        # 
+        piece = None
+        pieces = {
+            'R' : Rook, 
+            'N' : Knight, 
+            'B' : Bishop, 
+            'Q' : Queen,
+            'K' : King, 
+            'P' : Pawn,
+            'D' : 'dump'
+        }
+        if len(token) == 1:
+            p = token[0]
+            colour = 'White' if p.isupper() else 'Black'
+            p = p.upper()
+            if p in pieces:
+                if p == 'D':
+                    piece = ()  # empty
+                    print(f"Notation dump: {self.dump()}")
+                else:
+                    piece = (pieces[p], colour)
+        return piece
+    
+    def commands(self, command):
+        '''
+        Run command string fmt:: [<piece_creations>] <piece_moves>
+        e.g. 'Ke1; Qc3; ra1; d; Ke1:d2; ra1:a2' ## white king in check, dump, K moved, rook attacks K
+        where:
+            <piece_creations> :: <piece_creation> [; <piece_creations>]
+
+            <piece_creation> :: <piece_or_dump><pos> : to create a piece at pos
+            <piece_or_dump> :: <piece> | 'D' : where 'd' is string dump action
+            <piece> :: <piece_white> | <piece_black>
+            <piece_white> :: 'R' | 'N' | 'B' | 'Q' | 'K' | 'P' : upper case is white
+            <piece_black> :: 'r' | 'n' | 'b' | 'q' | 'k' | 'p' : lower case is black
+            <pos> :: <label> | <row_col> : two position formats
+            <label> :: <col_label> <row_label> : e.g. a1 == (7,0), h8 == (0, 7)
+            <row_col> :: '(<row>, <col>)' : board position tuple e.g. (0, 0) for r (black rook)
+            <col_label> :: '[a-h]' : 'a' == column 0, 'h' == column 7
+            <row_label> :: '[1-8]' : 1 == row 7, 8 == row 0
+
+            <piece_moves> :: <piece_move> [; <piece_moves>]
+            <piece_move> :: <piece> <from_pos>:<to_pos>
+            <from_pos> :: <pos> : move from position
+            <to_pos> :: <pos> : move to position
+        '''
+        errs = []
+        tokens = command.split(';')
+        ncommands = len(tokens)
+        for (indext, token) in enumerate(tokens):
+            token = re.sub(r'\s+', '', token) # remove whitespace
+            m = re.match(r'[Dd]$', token)
+            if m:
+                print(f"Notation dump: {self.dump()}")
+                continue
+
+            pieces = 'RNBQKP'
+            pieces_fmt = f'[{pieces}{pieces.lower()}]'
+            pos_fmt = r'\([0-7],[0-7]\)|[A-ha-h][1-8]'
+            m = re.match(f'({pieces_fmt})({pos_fmt})\:?({pos_fmt})?$', token)
+            groups = m.groups() if m else None
+            #print(f"DBG token {token} == {groups}")
+            if groups == None or (groups[0] == None and groups[-1] == None):
+                err_mess = f"invalid command token index [{indext}]:{token}"
+                errs.append((indext, err_mess))
+                continue
+
+            np = self.notation_piece(groups[0])
+            if np == None:
+                continue
+            from_pos = self.notation_pos(groups[1])
+            piece = self.get_piece(*from_pos)
+            to_pos = None
+            if groups[2]:
+                to_pos = self.notation_pos(groups[2])
+
+            if to_pos:
+                if piece and piece.abbrv == groups[0]:
+                    # move e.g. Ke1:d2; expects match piece at from_pos:
+                    try:
+                        self.move(from_pos, to_pos)
+                    except ChessExc as exc:
+                        err_mess = f"command token index [{indext}]:{token} move({from_pos}, {to_pos}) raised an exception {exc}"
+                        errs.append((indext, err_mess))
+                else:
+                    err_mess = f"invalid command token index [{indext}]:{token} mismatch move {piece} ({from_pos}, {to_pos}"
+                    errs.append((indext, err_mess))
+            else:
+                np = None
+                if piece == None:
+                    # New piece only if square empty
+                    np = self.notation_piece(token[0])
+                if np:
+                    # create new piece
+                    self.board[from_pos[0]][from_pos[1]] = \
+                        np[0](np[1], from_pos[1], from_pos[0])
+                else:
+                    err_mess = f"invalid command token index [{indext}]:{token}, existing piece {piece.piece} {from_pos}"
+                    errs.append((indext, err_mess))
+
+        return (ncommands, errs)
 
     def read_game_data(self, file):
-        turn = None
-        board = [[None]*8 for row in range(8)]
-        input_data = open(f'Games/{file}', 'r').readlines()
-        for i, line in enumerate(input_data):
-            if i == 0: turn = int(line.rstrip())
-            else:
-                Piece, Colour, Row, Column = line.rstrip().split(' ')
-                piece = eval(Piece+'(str(Colour), int(Column), int(Row))')
-                board[int(piece.row)][int(piece.column)] = piece 
+        turn = 0
+        board = self.new_board()
+        if file:
+            input_data = open(f'Games/{file}', 'r').readlines()
+            for i, line in enumerate(input_data):
+                if i == 0: turn = int(line.rstrip())
+                else:
+                    Piece, Colour, Row, Column = line.rstrip().split(' ')
+                    piece = eval(Piece+'(str(Colour), int(Column), int(Row))')
+                    board[int(piece.row)][int(piece.column)] = piece 
         return board, turn
     
     def check_against_check(self, clicked_piece):
