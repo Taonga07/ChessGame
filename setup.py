c3c1e23cf0b120e3ead0ec1da18de7c16277c264
taonga07 <thomas-2415@digilocal.org.uk>
2021-10-23 18:27:59 +0100

    added setup files

---
+++ setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+setup(
+  name = 'Chess2030',
+  packages = ['Chess2030'],
+  version = '0.1',
+  license='MIT',
+  description = 'tkinter chess game',
+  author = 'taonga07',
+  author_email = 'taongaws07@gmail.com',
+  url = 'https://github.com/Taonga07/Chess2030',
+  download_url = 'https://github.com/Taonga07/Chess2030/archive/refs/tags/v_1.tar.gz',
+  keywords = ['Chess', 'Python3', 'tkinter'],
+  install_requires=[],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
