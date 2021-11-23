79930dad8190f072ba23eeab2c6ee82778162596
taonga07 <thomas-2415@digilocal.org.uk>
2021-11-23 19:03:51 +0000

    edited oder of imports for Main.py

--- Main.py
+++ Main.py
@@ -1,12 +1,13 @@
 # pylint: disable=wildcard-import, C0413
-from ChessGame.Headless import Headless_ChessGame
-from ChessGame.Gui import Gui_ChessGame
+# fmt: off
 from os.path import join, dirname
 import sys
 
 sys.path.append(join(dirname(__file__), "ChessGame"))
 
-
+from ChessGame.Headless import Headless_ChessGame
+from ChessGame.Gui import Gui_ChessGame
+# fmt: on
 if __name__ == "__main__":
     Gui = Gui_ChessGame(Headless_ChessGame())
     Gui.root_window.mainloop()
